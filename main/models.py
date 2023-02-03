c447fb2d69bb240f62c9ecb198fe9deb080924cf
baiel <alimbekovbayellll@gmail.com>
2023-02-03 20:52:50 +0600

    baza

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Hospital(models.Model):
+    okpo = models.CharField(max_length=222, unique=True)
+    chiefphysician = models.CharField(max_length=222, unique=True)
+    hirurg =  models.CharField(max_length=222)
+    therapists = models.CharField(max_length=222)
+    nurses = models.CharField(max_length=222)
+    patients = models.CharField(max_length=222)
+
+
+
+
+
+    def __str__(self):
+        return self.okpo
